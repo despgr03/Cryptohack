18015eec42bd75f05d0c907806cb79b2f769ad1c
DESPOINA DESPOINIDI <99815072+despgr03@users.noreply.github.com>
2025-09-28 11:08:23 +0300

    Update Ascii.py

--- General/Encoding/Ascii.py
+++ General/Encoding/Ascii.py
@@ -2,7 +2,8 @@ import base64
 from Crypto.Util.number import *
 
 #for each number in the array that depicts an ascii character,we convert it using the chr() array.
-#solution:crypto{ASCII_pr1nt4bl3}
 t=[99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
 for i in t:
     print(chr(i),end='')
+
+#solution:crypto{ASCII_pr1nt4bl3}
