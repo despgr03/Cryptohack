eba75130bb653da65e345c997a33a847ce93e0aa
DESPOINA DESPOINIDI <99815072+despgr03@users.noreply.github.com>
2025-09-28 12:28:46 +0300

    Create BytesandBigIntegers.py

---
+++ General/Encoding/BytesandBigIntegers.py
@@ -0,0 +1,9 @@
+import base64
+from Crypto.Util.number import *
+
+#we convert the long to bytes
+t=11515195063862318899931685488813747395775516287289682636499965282714637259206269
+t1=long_to_bytes(t)
+print(t1)
+
+#solution:crypto{3nc0d1n6_4ll_7h3_w4y_d0wn}
