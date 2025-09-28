3ccfe2c2a98a1edc9ab295fe2f534d73555bc295
DESPOINA DESPOINIDI <99815072+despgr03@users.noreply.github.com>
2025-09-28 12:43:10 +0300

    Update XORStarter.py

--- General/XOR/XORStarter.py
+++ General/XOR/XORStarter.py
@@ -6,7 +6,7 @@ t='label'
 t2=''
 #for each character in t, we convert it to the ascii number and then we do xor the number with the number 13 and the number we turn it into a character
 for i in t:
-    t2+=chr(int((xor(ord(i),13))))
+    t2+=chr((xor(ord(i),13)))
 print(t2)
 
 #solution:aloha
