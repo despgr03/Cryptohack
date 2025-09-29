889fb90c0378fe8617d5d95708f359c05e79b6ba
DESPOINA DESPOINIDI <99815072+despgr03@users.noreply.github.com>
2025-09-29 11:38:52 +0300

    Create Modular Inverting.py

---
+++ General/Mathematics/Modular Inverting.py
@@ -0,0 +1,13 @@
+def modar1(a,b): 
+    i=1 
+  #3*d≡1mod13 is ((3*d)-1)%13==0 so in order to find the d we increase the i until the condition is satisfied.
+    while(True): 
+        if((((a*i)-1)%b)==0): 
+            break 
+        else: 
+            i+=1 
+    print(i) 
+ 
+modar1(3,13) 
+
+#solution: 9
