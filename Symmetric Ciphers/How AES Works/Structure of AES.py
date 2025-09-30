5ee5da95c581dd6fa82974ae7966d4499379b792
DESPOINA DESPOINIDI <99815072+despgr03@users.noreply.github.com>
2025-09-30 12:05:00 +0300

    Create Structure of AES.py

---
+++ Symmetric Ciphers/How AES Works/Structure of AES.py
@@ -0,0 +1,21 @@
+def bytes2matrix(text):
+    """ Converts a 16-byte array into a 4x4 matrix.  """
+    return [list(text[i:i+4]) for i in range(0, len(text), 4)]
+
+#for every element in the matrix we convert it in its ascii character
+def matrix2bytes(matrix):
+    t=""
+    for i in matrix:
+        for j in i:
+            t+=chr(j)
+    return t
+        
+
+matrix = [
+    [99, 114, 121, 112],
+    [116, 111, 123, 105],
+    [110, 109, 97, 116],
+    [114, 105, 120, 125],
+]
+
+print(matrix2bytes(matrix))
