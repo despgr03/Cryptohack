4ccec97d75eb1e358d6b426127db81ca89d90588
DESPOINA DESPOINIDI <99815072+despgr03@users.noreply.github.com>
2025-10-01 13:26:36 +0300

    Create Euler's Totient.py

---
+++ RSA/Starter/Euler's Totient.py
@@ -0,0 +1,6 @@
+p = 857504083339712752489993810777 
+q = 1029224947942998075080348647219 
+t=(p-1)*(q-1) 
+print(t) 
+
+#according to the Euler’s Totient Function Φ(Ν)=(p-1)(q-1) where p and q are two prime numbers
