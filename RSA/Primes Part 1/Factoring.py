d45cc0f4e26c267c07f00aaf0910ac4a35381166
DESPOINA DESPOINIDI <99815072+despgr03@users.noreply.github.com>
2025-10-01 13:47:43 +0300

    Create Factoring.py

---
+++ RSA/Primes Part 1/Factoring.py
@@ -0,0 +1,5 @@
+from sympy import primefactors 
+n=primefactors(510143758735509025530880200653196460532653147) 
+print(n)
+
+#we use the method primefactors from sympy and it gives us the two prime numbers that their products is the number we have given
